72bb828fa900f1410c08396fd9800388f6baa05b
hinst <hinstance@yandex.ru>
2017-03-14 15:23:08 +0300

    http server in progress

--- HCommitCa/App.h
+++ HCommitCa/App.h
@@ -1,9 +1,20 @@
-#include "http_async_server.hpp"
+#pragma once
+
+#include "http_sync_server.hpp"
+
+namespace hca { namespace App {
+
+using namespace std;
 
 class App {
 	
+	string appDir = "";
+	
 	void run() {
-		
+		auto endpoint = boost::asio::ip::tcp::endpoint{boost::asio::ip::address::from_string("0.0.0.0"), 8080};
+		auto server = beast::http::http_sync_server(endpoint);
 	}
 	
 };
+
+}}
--- HCommitCa/main.cpp
+++ HCommitCa/main.cpp
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include "App.h"
 
+using namespace hca::App;
+
 int main(int argc, char *argv[])
 {
 	printf("hello world\n");
